934a9803e629db1bc92d69fdaf1d2d1013053dd9
s1lv3r10 <isabelly.silverio@etec.sp.gov.br>
2025-04-22 10:47:50 -0300

    [geral] react-navigation iniciado

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+
+const Stack = createNativeStackNavigator<{Gincanews: undefined}, 'Nav'>()
+
+function RootStack() {
+  return (
+    <Stack.Navigator id='Nav'>
+      <Stack.Screen name='Gincanews' component={Home} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
---
+++ components/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Home() {
+    return (
+        <View style={{margin: 50}}>
+            <Text>Home</Text>
+        </View>
+    )
+}
