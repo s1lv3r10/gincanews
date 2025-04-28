c22fc2f33c1c27c001530ba33b134d16376f957a
s1lv3r10 <isabelly.silverio@etec.sp.gov.br>
2025-04-28 10:45:56 -0300

    user page

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
+import User from './components/User';
 
-const Stack = createNativeStackNavigator<{Gincanews: undefined}, 'Nav'>()
+const Stack = createBottomTabNavigator<{
+  Gincanews: undefined,
+  User: undefined
+}, 'Nav'>()
 
 function RootStack() {
   return (
     <Stack.Navigator id='Nav'>
       <Stack.Screen name='Gincanews' component={Home} />
+      <Stack.Screen name='User' component={User} />
     </Stack.Navigator>
   )
 }
---
+++ components/User.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function User() {
+    return (
+        <View style={{margin: 50}}>
+            <Text>Usuário</Text>
+        </View>
+    )
+}
