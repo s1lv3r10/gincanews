6c2634f7d27ec5295449c0b4906df5ac7442bedf
s1lv3r10 <isabelly.silverio@etec.sp.gov.br>
2025-06-15 19:06:55 -0300

    home quase terminada

--- utils/styles.tsx
+++ utils/styles.tsx
@@ -20,5 +20,48 @@ export const ContainerStyles = StyleSheet.create({
         alignItems: 'center',
         padding: 30, 
         gap: 20,
+    },
+    cardEvento: { //Cards de datas
+        width: 180,
+        margin: 3,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    pic:{
+        height: 100
+    },
+    homeButton:{
+        backgroundColor: '#911919',
+        margin:10,
+        padding:5,
+        borderRadius: 100
+    },
+    horizontalRule: {
+        borderBottomColor: '#911919',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginVertical: 10,
+    },
+    Title:{
+        color:'000',
+        fontSize: 27,
+        fontWeight: 'bold',
+        textAlign:'center'
+    },
+    subTitle:{
+        color:'rgb(146, 146, 146)',
+        fontSize: 16,
+        textAlign:'center'
+    },
+    cardNews:{ //Cards de últimos eventos
+        margin: 5,
+    },
+    cardText:{
+        textAlign: 'center'
+    },
+    cardDate:{
+        fontSize: 20,
+        textAlign:'center'
     }
-})
+});
